97e34e142b4652a57c44a25b42535fdcb5f08222
DevExpressExampleBot <fakeEmail>
2020-03-09 16:07:51 +0300

    Source auto update [skip ci]

---
+++ VB/PrivateFontDemo/MainWindow.xaml.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Text
+Imports System.Linq
+Imports System.Windows
+Imports DevExpress.Utils.Serializing
+Imports DevExpress.XtraPrinting.Caching
+
+Namespace PrivateFontDemo
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+
+		Shared Sub New()
+			PrintingSystemXmlSerializer.UnregisterConverter(GetType(Font))
+			PrintingSystemXmlSerializer.RegisterConverter(New CustomFontConverter())
+		End Sub
+
+
+		Public Sub New()
+			InitializeComponent()
+			AddHandler Me.Loaded, AddressOf MainWindow_Loaded
+		End Sub
+
+		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim report = New SampleReport()
+			report.Font = New Font(CustomFontsHelper.GetFamily("Miss Fajardose"), 48F, System.Drawing.FontStyle.Regular, GraphicsUnit.Point)
+
+			Dim cachedReportSource As New CachedReportSource(report, New MemoryDocumentStorage())
+			preview.DocumentSource = cachedReportSource
+			cachedReportSource.CreateDocumentAsync()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/PrivateFontDemo/SampleReport.vb
@@ -0,0 +1,66 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports DevExpress.XtraReports.UI
+
+Namespace PrivateFontDemo
+	Public Class SampleReport
+		Inherits XtraReport
+
+		Private Detail As DetailBand
+		Private lbText As XRLabel
+		Private TopMargin As TopMarginBand
+		Private BottomMargin As BottomMarginBand
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.lbText = New DevExpress.XtraReports.UI.XRLabel()
+			DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.lbText})
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' TopMargin
+			' 
+			Me.TopMargin.Name = "TopMargin"
+			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' BottomMargin
+			' 
+			Me.BottomMargin.Name = "BottomMargin"
+			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' lbText
+			' 
+			Me.lbText.LocationFloat = New DevExpress.Utils.PointFloat(10.00001F, 10.00001F)
+			Me.lbText.Name = "lbText"
+			Me.lbText.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.lbText.SizeF = New System.Drawing.SizeF(630F, 79.99999F)
+			Me.lbText.StylePriority.UseFont = False
+			Me.lbText.StylePriority.UseTextAlignment = False
+			Me.lbText.Text = "Hello World"
+			Me.lbText.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
+			' 
+			' SampleReport
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin})
+			Me.Version = "18.2"
+			DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+	End Class
+End Namespace
