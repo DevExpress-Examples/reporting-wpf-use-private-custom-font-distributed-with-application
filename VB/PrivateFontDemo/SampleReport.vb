bb157f2d5bf89c11b7e23041e9e2134de89e6749
DevExpressExampleBot <fakeEmail>
2022-09-05 19:05:59 +0400

    Source auto update [skip ci]

--- VB/PrivateFontDemo/SampleReport.vb
+++ VB/PrivateFontDemo/SampleReport.vb
@@ -1,66 +1,64 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports DevExpress.XtraReports.UI
 
 Namespace PrivateFontDemo
-	Public Class SampleReport
-		Inherits XtraReport
 
-		Private Detail As DetailBand
-		Private lbText As XRLabel
-		Private TopMargin As TopMarginBand
-		Private BottomMargin As BottomMarginBand
+    Public Class SampleReport
+        Inherits XtraReport
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Private Detail As DetailBand
 
-		Private Sub InitializeComponent()
-			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
-			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
-			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
-			Me.lbText = New DevExpress.XtraReports.UI.XRLabel()
-			DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
-			' 
-			' Detail
-			' 
-			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.lbText})
-			Me.Detail.Name = "Detail"
-			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
-			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
-			' 
-			' TopMargin
-			' 
-			Me.TopMargin.Name = "TopMargin"
-			Me.TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
-			Me.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
-			' 
-			' BottomMargin
-			' 
-			Me.BottomMargin.Name = "BottomMargin"
-			Me.BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
-			Me.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
-			' 
-			' lbText
-			' 
-			Me.lbText.LocationFloat = New DevExpress.Utils.PointFloat(10.00001F, 10.00001F)
-			Me.lbText.Name = "lbText"
-			Me.lbText.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
-			Me.lbText.SizeF = New System.Drawing.SizeF(630F, 79.99999F)
-			Me.lbText.StylePriority.UseFont = False
-			Me.lbText.StylePriority.UseTextAlignment = False
-			Me.lbText.Text = "Hello World"
-			Me.lbText.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
-			' 
-			' SampleReport
-			' 
-			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.TopMargin, Me.BottomMargin})
-			Me.Version = "18.2"
-			DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Private lbText As XRLabel
 
-		End Sub
-	End Class
+        Private TopMargin As TopMarginBand
+
+        Private BottomMargin As BottomMarginBand
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub InitializeComponent()
+            Detail = New DetailBand()
+            TopMargin = New TopMarginBand()
+            BottomMargin = New BottomMarginBand()
+            lbText = New XRLabel()
+            CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' Detail
+            ' 
+            Detail.Controls.AddRange(New XRControl() {lbText})
+            Detail.Name = "Detail"
+            Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' TopMargin
+            ' 
+            TopMargin.Name = "TopMargin"
+            TopMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' BottomMargin
+            ' 
+            BottomMargin.Name = "BottomMargin"
+            BottomMargin.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+            BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+            ' 
+            ' lbText
+            ' 
+            lbText.LocationFloat = New DevExpress.Utils.PointFloat(10.00001F, 10.00001F)
+            lbText.Name = "lbText"
+            lbText.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+            lbText.SizeF = New System.Drawing.SizeF(630F, 79.99999F)
+            lbText.StylePriority.UseFont = False
+            lbText.StylePriority.UseTextAlignment = False
+            lbText.Text = "Hello World"
+            lbText.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter
+            ' 
+            ' SampleReport
+            ' 
+            Bands.AddRange(New Band() {Detail, TopMargin, BottomMargin})
+            Version = "18.2"
+            CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        End Sub
+    End Class
 End Namespace
